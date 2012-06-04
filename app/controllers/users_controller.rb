044ad7b98833cb0954f2cd12ee2b88ab4adbebae
Ray Wu <rwu721@gmail.com>
2012-06-04 00:31:15 -0700

    finish user edit, update, index, and destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+	 before_filter :signed_in_user, only: [:index, :edit, :update]
+	 before_filter :correct_user,   only: [:edit, :update]
+	 before_filter :admin_user,     only: :destroy
   
   def new
     @user = User.new
@@ -20,9 +23,45 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    sign_out
-    redirect_to root_path
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_path
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    if @user.update_attributes(params[:user])
+      sign_in @user
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
+	private
+
+    	def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_path, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
 
 end
